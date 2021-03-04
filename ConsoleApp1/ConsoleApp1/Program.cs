e71896ea4de209b0ab2798acaf718944a68d6ecb
Gusta-bit <77754564+Gusta-bit@users.noreply.github.com>
2021-03-04 15:55:22 -0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "incomodam";
+            int numero = 0;
+
+            Console.WriteLine("Digite um numero");
+            numero = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numero; i++)
+            {
+
+                Console.WriteLine("Um elefante " + a + " Muita gente");
+            }
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            int maior = 0, menor = 0, aux = 0, i = 0;
+            do
+            {
+                Console.WriteLine("Digite um numero");
+                aux = int.Parse(Console.ReadLine());
+                if (i == 0)
+                {
+                    maior = aux;
+                    menor = aux;
+                    i++;
+                }
+                 else if (aux > maior)
+                {
+                    maior = aux;
+                }
+                else if (aux < menor)
+                {
+                    menor = aux;
+                }
+            } while (1);
+
+            Console.WriteLine("O Maior numero: " + maior + " o menor numero " + menor);
+        }
+    }
+}
